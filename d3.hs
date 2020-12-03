37d3bf79dc063d1702d3122534f7902d3489dd8d
tuckerrrrrrrrrr <tucker1014@hotmail.com>
2020-12-02 22:43:08 -0800

    d3: solve p1 and p2 at the same time

--- d3.hs
+++ d3.hs
@@ -5,10 +5,13 @@ treesHit (x, y) (dx, dy) m
         where hit = fromEnum ((m !! y) !! x == '#')
               nx  = mod (x + dx) $ length (m !! 0)
 
-slopes :: [(Int, Int)]
-slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
-
 solve :: [(Int, Int)] -> [String] -> Int
 solve ss m = product $ map (\s -> treesHit (0, 0) s m) ss
 
-main = interact $ show . solve slopes . lines
+slopes :: [(Int, Int)]
+slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+
+main = do
+        inp <- getContents
+        putStrLn $ (++) "Silver: " $ show $ solve [(3, 1)] $ lines inp
+        putStrLn $ (++) "Gold:   " $ show $ solve slopes $ lines inp
