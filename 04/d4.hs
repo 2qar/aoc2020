e3f8a9c34af9164a3bf1cc3e2bd2093690c67751
tuckerrrrrrrrrr <tucker1014@hotmail.com>
2020-12-03 21:59:24 -0800

    day 4 kinda messy

---
+++ 04/d4.hs
@@ -0,0 +1,75 @@
+import Data.List
+import Text.Printf
+
+contains :: String -> String -> Bool
+contains sub s
+        | (length sub) > (length s) = False
+        | otherwise                 = (isPrefixOf sub s) || (contains sub $ tail s)
+
+valid1 :: String -> Int
+valid1 s = fromEnum
+        $ (contains "byr:" s) &&
+          (contains "iyr:" s) &&
+          (contains "eyr:" s) &&
+          (contains "hgt:" s) &&
+          (contains "hcl:" s) &&
+          (contains "ecl:" s) &&
+          (contains "pid:" s)
+
+validByr :: Int -> Bool
+validByr y = (length $ show y) == 4 && y >= 1920 && y <= 2002
+
+validIyr :: Int -> Bool
+validIyr y = (length $ show y) == 4 && y >= 2010 && y <= 2020
+
+validEyr :: Int -> Bool
+validEyr y = (length $ show y) == 4 && y >= 2020 && y <= 2030
+
+validHgt :: String -> Bool
+validHgt s
+    | (contains "cm" s) = n >= 150 && n <= 193
+    | (contains "in" s) = n >= 59 && n <= 76
+    | otherwise         = False
+    where n = read $ takeWhile (\c -> c `elem` ['0'..'9']) s
+
+validHcl :: String -> Bool
+validHcl s = (head s) == '#' && (length [c | c <- s, c `elem` ['0'..'9'] || c `elem` ['a'..'f']]) == 6
+
+validEcl :: String -> Bool
+validEcl s = s `elem` ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+
+validPid :: String -> Bool
+validPid s = (length [c | c <- s, c `elem` ['0'..'9']]) == 9
+
+validFields :: [String] -> Bool
+validFields fs
+        | null fs    = True -- bad
+        | f == "byr" = (validByr $ read v) && validFields n
+        | f == "iyr" = (validIyr $ read v) && validFields n
+        | f == "eyr" = (validEyr $ read v) && validFields n
+        | f == "hgt" = (validHgt v) && validFields n
+        | f == "hcl" = (validHcl v) && validFields n
+        | f == "ecl" = (validEcl v) && validFields n
+        | f == "pid" = (validPid v) && validFields n
+        | f == "cid" = validFields n
+        | otherwise  = False
+        where f = takeWhile (\c -> c /= ':') (head fs)
+              v = drop 1 $ dropWhile (\c -> c /= ':') (head fs)
+              n = tail fs
+
+valid2 :: String -> Int
+valid2 s = fromEnum $ (toEnum $ valid1 s) && (validFields $ words s)
+
+solve :: (String -> Int) -> [String] -> Int
+solve valid s
+    | null s    = 0
+    | otherwise = (valid c) + solve valid n
+    where c = unwords $ takeWhile (\l -> not (null l)) s
+          n = drop 1 $ dropWhile (\l -> not (null l)) s
+
+main = do 
+    s <- getContents
+    let ps = lines s
+
+    printf "Silver: %d\n" $ solve valid1 ps
+    printf "Gold:   %d\n" $ solve valid2 ps
