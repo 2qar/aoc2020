9a76ac65fa08bc30eb0e93f44891f7977be8a71f
tuckerrrrrrrrrr <tucker1014@hotmail.com>
2020-12-03 18:01:06 -0800

    d1: solve p1 + p2 at the same time

--- d1.hs
+++ d1.hs
@@ -1,9 +1,15 @@
+import Text.Printf
 import Control.Monad
 
 solve :: Int -> [Int] -> Int
 solve n ns = product 
                 $ head
-		$ filter (\l -> (sum l) == 2020)
-		$ replicateM n ns
+                $ filter (\l -> (sum l) == 2020)
+                $ replicateM n ns
 
-main = interact $ show . solve 2 . map read . lines
+main = do
+    ss <- getContents
+    let ns = [read n | n <- (lines ss)]
+
+    printf "Silver: %d\n" $ solve 2 ns
+    printf "Gold:   %d\n" $ solve 3 ns
