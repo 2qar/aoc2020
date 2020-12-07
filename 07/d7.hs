5ba9c86110a7a52629df79d5591f80bf3ab78e35
tuckerrrrrrrrrr <tucker1014@hotmail.com>
2020-12-06 22:22:33 -0800

    d7 part 2 (it's pretty slow)

--- 07/d7.hs
+++ 07/d7.hs
@@ -1,22 +1,19 @@
 import Data.List
 import Data.Maybe
 import Text.Printf
--- tree: light red    -> (bright white, muted yellow)
---       bright white -> (shiny gold, none)
---       muted yellow -> (shiny gold, faded blue)
---       faded blue   -> (none, none)
 
-type Rule = (String, [String])
+type Rule = (String, [(Int, String)])
 
 hasSep :: String -> Bool
 hasSep w = (elem ',' w) || (elem '.' w)
 
-parseRules :: [String] -> [String]
+parseRules :: [String] -> [(Int, String)]
 parseRules ws
     | null ws           = []
     | (head ws) == "no" = []
-    | otherwise         = [(unwords r)] ++ (parseRules n)
-    where r = takeWhile (\w -> not $ hasSep w) $ drop 1 ws
+    | otherwise         = [(read i, unwords r)] ++ (parseRules n)
+    where i = head ws
+          r = takeWhile (\w -> not $ hasSep w) $ drop 1 ws
           n = drop 1 $ dropWhile (\w -> not $ hasSep w) ws
 
 parseRule :: [String] -> Rule
@@ -29,12 +26,22 @@ findAndCheck rs n = maybe False (\n -> hasShinyGold rs n) $ find (\r -> (fst r) 
 
 hasShinyGold :: [Rule] -> Rule -> Bool
 hasShinyGold rs r 
-    | elem "shiny gold" $ snd r = True
-    | otherwise                 = elem True $ map (\n -> findAndCheck rs n) $ snd r
+    | elem "shiny gold" $ map snd $ snd r = True
+    | otherwise                           = elem True $ map (\r -> findAndCheck rs $ snd r) $ snd r
+
+findRule :: [Rule] -> String -> Rule
+findRule rs n = maybe ("", []) (\r -> r) $ find (\r -> (fst r) == n) rs
+
+bagsIn :: [Rule] -> Rule -> Int
+bagsIn rs r
+    | null nrs  = 0
+    | otherwise = sum $ map (\r -> (fst r) + (fst r) * (bagsIn rs $ findRule rs $ snd r)) nrs
+    where nrs = snd r
 
 main = do
     s <- getContents
     let rules = map (\l -> parseRule $ words l) $ lines s
-    print rules
 
     printf "Silver: %d\n" $ sum $ map (\r -> fromEnum $ hasShinyGold rules r) rules
+    let shiny = fromJust $ find (\r -> fst r == "shiny gold") rules
+    printf "Gold:   %d\n" $ bagsIn rules shiny
