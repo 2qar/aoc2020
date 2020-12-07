6f81eb04a72a985988f9ebbe5c90de719e9560fc
tuckerrrrrrrrrr <tucker1014@hotmail.com>
2020-12-06 23:00:16 -0800

    d7: use a Map cus its faster

--- 07/d7.hs
+++ 07/d7.hs
@@ -1,8 +1,10 @@
 import Data.List
 import Data.Maybe
+import qualified Data.Map.Strict as Map
 import Text.Printf
 
-type Rule = (String, [(Int, String)])
+type Rule = [(Int, String)]
+type Rules = Map.Map String Rule
 
 hasSep :: String -> Bool
 hasSep w = (elem ',' w) || (elem '.' w)
@@ -16,32 +18,30 @@ parseRules ws
           r = takeWhile (\w -> not $ hasSep w) $ drop 1 ws
           n = drop 1 $ dropWhile (\w -> not $ hasSep w) ws
 
-parseRule :: [String] -> Rule
+parseRule :: [String] -> (String, Rule)
 parseRule ws = (n, parseRules rs)
     where n  = unwords $ take 2 $ takeWhile (\l -> l /= "contain") ws
           rs = drop 1 $ dropWhile (\w -> w /= "contain") ws
 
-findAndCheck :: [Rule] -> String -> Bool
-findAndCheck rs n = maybe False (\n -> hasShinyGold rs n) $ find (\r -> (fst r) == n) rs
+findAndCheck :: Rules -> String -> Bool
+findAndCheck rs n = maybe False (\r -> hasShinyGold rs r) $ Map.lookup n rs
 
-hasShinyGold :: [Rule] -> Rule -> Bool
+hasShinyGold :: Rules -> Rule -> Bool
 hasShinyGold rs r 
-    | elem "shiny gold" $ map snd $ snd r = True
-    | otherwise                           = elem True $ map (\r -> findAndCheck rs $ snd r) $ snd r
+    | elem "shiny gold" $ map snd r = True
+    | otherwise                     = elem True $ map (\r -> findAndCheck rs $ snd r) r
 
-findRule :: [Rule] -> String -> Rule
-findRule rs n = maybe ("", []) (\r -> r) $ find (\r -> (fst r) == n) rs
+findRule :: Rules -> String -> Rule
+findRule rs n = maybe [] (\r -> r) $ Map.lookup n rs
 
-bagsIn :: [Rule] -> Rule -> Int
+bagsIn :: Rules -> Rule -> Int
 bagsIn rs r
-    | null nrs  = 0
-    | otherwise = sum $ map (\r -> (fst r) + (fst r) * (bagsIn rs $ findRule rs $ snd r)) nrs
-    where nrs = snd r
+    | null r    = 0
+    | otherwise = sum $ map (\r -> (fst r) + (fst r) * (bagsIn rs $ findRule rs $ snd r)) r
 
 main = do
     s <- getContents
-    let rules = map (\l -> parseRule $ words l) $ lines s
+    let rules = Map.fromList $ map (\l -> parseRule $ words l) $ lines s
 
-    printf "Silver: %d\n" $ sum $ map (\r -> fromEnum $ hasShinyGold rules r) rules
-    let shiny = fromJust $ find (\r -> fst r == "shiny gold") rules
-    printf "Gold:   %d\n" $ bagsIn rules shiny
+    printf "Silver: %d\n" $ sum $ Map.map (\r -> fromEnum $ hasShinyGold rules r) rules
+    printf "Gold:   %d\n" $ bagsIn rules $ fromJust $ Map.lookup "shiny gold" rules
